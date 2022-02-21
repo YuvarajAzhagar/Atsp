a3f0c61530b980ec407741a5a90f815895ae7d3b
YuvarajAzhagar <yuvarajazhagar@gmail.com>
2022-02-21 20:14:32 +0530

    Yuvaraaj

---
+++ src/org/tamil/tamill.java
@@ -0,0 +1,13 @@
+package org.tamil;
+
+public class tamill {
+	
+	private void Tamil() {
+		System.out.println("Tamil");
+	}
+
+	public static void main(String[] args) {
+		tamill a = new tamill();
+			a.Tamil();
+	}
+}
